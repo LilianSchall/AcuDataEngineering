87e263181ea0096275106a5607509c4a675593f8
Emile <emile.merle@epita.fr>
2024-06-21 16:22:22 +0200

    feat: create alert system with kafka-connect

---
+++ kafka-connect-alert/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+/etc/confluent/docker/run &
+
+while ! curl -s localhost:8083; do
+    echo "Waiting for Kafka connect to start..."
+    sleep 5
+done
+
+curl -X POST -H "Content-Type: application/json" --data @/etc/kafka-connect/kafka-alert-connector.json http://localhost:8083/connectors
+
+wait
