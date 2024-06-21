dad43760c1404fb2eae0e30f7dbd15794f0fbbb9
julien.schaffauser <julien.schaffauser@epita.fr>
2024-06-21 16:18:15 +0200

    feat: add first version of studentsignal data generator class

--- StudentsSignal/build.sbt
+++ StudentsSignal/build.sbt
@@ -1,9 +1,10 @@
 ThisBuild / version := "0.1.0-SNAPSHOT"
 
-ThisBuild / scalaVersion := "2.13.3"
+ThisBuild / scalaVersion := "2.13.11"
 
 lazy val root = (project in file("."))
   .settings(
-    name := "StudentsSignal",
-    libraryDependencies += "org.apache.kafka" % "kafka-clients" % "3.7.0"
-  )
+      name := "StudentsSignal",
+      libraryDependencies += "org.apache.kafka" % "kafka-clients" % "3.7.0",
+        libraryDependencies += "com.lihaoyi" %% "ujson" % "3.1.1"
+)
---
+++ StudentsSignal/src/main/scala/StudentSignal.scala
@@ -0,0 +1,76 @@
+import scala.io.Source
+import scala.util.Random
+import ujson._
+
+object StudentSignal {
+  case class StudentSignal(
+                            login: String,
+                            latitude: Double,
+                            longitude: Double,
+                            exercise: Int,
+                            score: Int,
+                          )
+
+  def readLinesFromFile(fileName: String): List[String] = {
+    val source = Source.fromFile(fileName)
+    val lines = try source.getLines().toList finally source.close()
+    lines
+  }
+
+  def getRandomName(list: List[String]): String = {
+    val randomIndex = Random.nextInt(list.length)
+    list(randomIndex)
+  }
+
+  def getRandomElement(value: ujson.Value) : ujson.Value = {
+    val list = value.arr
+    val randomIndex = Random.nextInt(list.length)
+    list(randomIndex)
+  }
+
+  def createLogin() : String = {
+    val firstNames = readLinesFromFile("src/main/resources/firstname.txt")
+    val lastNames = readLinesFromFile("src/main/resources/lastname.txt")
+
+    val randomFirstName = getRandomName(firstNames)
+    val randomLastName = getRandomName(lastNames)
+
+    s"$randomFirstName.$randomLastName"
+  }
+
+  def createCoords() : (Double, Double) = {
+    val source = Source.fromFile("src/main/resources/coords.json")
+    val jsonString  = source.mkString
+    source.close()
+
+    val json = ujson.read(jsonString)
+    val regions = json("regions")
+
+    val randomRegion = getRandomElement(regions)
+
+    val randomCity = getRandomElement(randomRegion("cities"))
+
+    val latitude = randomCity("latitude").num
+    val longitude = randomCity("longitude").num
+
+    (latitude, longitude)
+  }
+
+  def createExercise(): Int = {
+    Random.nextInt(25)
+  }
+
+  def createScore(exercise: Int): Int = {
+    Random.nextInt(100)
+  }
+
+  def createStudentSignal(): StudentSignal = {
+    val login = createLogin()
+    val (latitude, longitude) = createCoords()
+
+    val exercise = createExercise()
+    val score = createScore(exercise)
+
+    StudentSignal(login, latitude, longitude, exercise, score)
+  }
+}
