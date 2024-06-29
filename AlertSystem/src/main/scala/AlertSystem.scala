fc3cbecbe3d9c83c99be716798833811e1ab3640
Emile <emile.merle@epita.fr>
2024-06-29 18:44:41 +0200

    feat: alert system for fix floor

--- AlertSystem/src/main/scala/AlertSystem.scala
+++ AlertSystem/src/main/scala/AlertSystem.scala
@@ -2,8 +2,22 @@ import org.apache.kafka.clients.consumer.{ConsumerConfig, KafkaConsumer}
 import org.apache.kafka.clients.producer.{KafkaProducer, ProducerConfig}
 import org.apache.kafka.common.serialization.{StringDeserializer, StringSerializer}
 
+import upickle.default._
+
 import java.util.Properties
 import scala.annotation.tailrec
+
+case class StudentSignal(
+                          login: String,
+                          latitude: Double,
+                          longitude: Double,
+                          exercise: Int,
+                          score: Int,
+                          timestamp: Long
+                        )
+object StudentSignal{
+  implicit val rw: RW[StudentSignal] = macroRW
+}
 
 class AlertSystem {
   private val consumerProps: Properties = new Properties()
@@ -28,7 +42,33 @@ class AlertSystem {
 
   @tailrec
   final def run(): Unit = {
-    ???
+    # consumer.get pour recup chaine de caractères
+    # serialiser avec upickle dans case class
+    # regarder score et evaluer selon seuil fixe
+    # seuil en variable d'environnement
+    # si score <= seuil, producer.send avec la string
+    # si score > seuil, rien à faire et appel recursif passe au message suivant
+    # push une fois que ça marche
+    # ensuite, faire un seuil en fonction de la difficulté
+    val records = consumer.poll(java.time.Duration.ofMillis(100)).asScala
+
+    for (record <- records) {
+      try {
+        val jsonString = record.value()
+        
+        val studentSignal = read[StudentSignal](jsonString)
+        
+        val floor = sys.env.getOrElse("SCORE_FLOOR", "20").toInt
+        if (studentSignal.score <= floor) {
+
+          val alertString = write(studentSignal)
+          val producerRecord = new ProducerRecord[String, String](alert, AlertString)
+          producer.send(producerRecord)
+        }
+      } catch {
+        case e: Exception => e.printStackTrace()
+      }
+    }
     run()
   }
 }
